213ebfdf02af401ced41bec7575219db835a77c0
Gheonea Emilian <gheoneaemilian@pop-os.localdomain>
2022-04-06 17:10:55 +0300

    withdrawable contract

---
+++ contracts/utils/Withdrawable.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract Withdrawable is Ownable {
+
+    function payout(uint256 amount) external onlyOwner {
+        require(amount <= address(this).balance, "Payout: Requested amount to withdraw is too big");
+        require(payable(owner()).send(amount), "Payout: Failed to withdraw");
+    }
+
+}
