01d493b4cd197255c9c63293e6597bc5c36912e4
Gheonea Emilian <gheoneaemilian@pop-os.localdomain>
2022-04-14 13:56:36 +0300

    logging improvements

--- plugins/logger.js
+++ plugins/logger.js
@@ -1,12 +1,18 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, prettyPrint } = format;
+const hardhat = require("hardhat")
 
+const deploymentFormat = format((info) => {
+  info.network = hardhat.network.name;
+  return info;
+})();
 
 const deployment = new createLogger({
   levels: {
     info: 1
   },
   format: combine(
+    deploymentFormat,
     timestamp(),
     prettyPrint()
   ),
@@ -15,9 +21,31 @@ const deployment = new createLogger({
   ]
 });
 
+const errorsFormat = format((info) => {
+  info.errorMessage = info.message;
+  return info;
+})();
+
+const errors = new createLogger({
+  levels: {
+    error: 3
+  },
+  format: combine(
+    errorsFormat,
+    timestamp(),
+    prettyPrint()
+  ),
+  transports: [
+    new (transports.File)({ filename: './errors.logs', level: 'error', timestamp: true })
+  ]
+});
+
 
 module.exports = {
   deployment: function(msg){
     deployment.info(msg);
+  },
+  errors: function(msg){
+    errors.error(msg);
   }
 };
