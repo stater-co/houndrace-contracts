17559d5de3edf8a9fdb4fc5e1a95af5fea42e460
DIIMIIM <emilian@stater.co>
2022-08-04 15:58:24 +0300

    logs added

---
+++ logs/deployment/printers/deployment.ts
@@ -0,0 +1,47 @@
+import { createLogger, format, Logger, LoggerOptions, transports } from 'winston';
+import { network } from 'hardhat';
+import userName from 'git-user-name';
+import { deploymentLogsPath } from '../../../common/paths';
+
+
+const deploymentFormat = format((info) => {
+  return {
+    ...info,
+    network: network.name
+  };
+})();
+
+const formats: Array<any> = [
+  deploymentFormat,
+  format.timestamp(),
+  format.prettyPrint(),
+  format.label({
+    label: String(userName()),
+    message: true
+  })
+];
+
+const loggerOptions: LoggerOptions = {
+  level: 'info',
+  format: format.combine(
+    ...formats
+  ),
+  transports: [
+    new (transports.File)({ 
+      filename: deploymentLogsPath.fileName, 
+      dirname: deploymentLogsPath.location, 
+      level: 'info', 
+      format: format.combine(
+        ...formats
+      )
+    })
+  ]
+};
+
+const logger: Logger = createLogger(loggerOptions);
+
+const DeploymentLogger = async (msg: string) => {
+  logger.info(msg);
+}
+
+export default DeploymentLogger;
