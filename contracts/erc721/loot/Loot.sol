96174e913588861490cbd2d094d138645a271e07
DIIMIIM <emilian@stater.co>
2022-09-16 19:13:35 +0300

    more opensea related updates

--- contracts/erc721/loot/Loot.sol
+++ contracts/erc721/loot/Loot.sol
@@ -78,4 +78,29 @@ contract Lootboxes is Ownable, ERC721URIStorage, ERC721Holder {
         return ERC721.isApprovedForAll(_owner, _operator);
     }
 
+    /**
+     * https://docs.opensea.io/docs/polygon-basic-integration
+     */
+    function _msgSender()
+        internal
+        override
+        view
+        returns (address sender)
+    {
+        if (msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(
+                    mload(add(array, index)),
+                    0xffffffffffffffffffffffffffffffffffffffff
+                )
+            }
+        } else {
+            sender = payable(msg.sender);
+        }
+        return sender;
+    }
+
 }
