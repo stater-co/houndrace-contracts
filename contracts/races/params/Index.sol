ecce41fd49474b3ea2c89e8eadf59b1e9459b579
DIIMIIM <emilian@stater.co>
2022-09-20 18:59:03 +0300

    not important changes

--- contracts/races/params/Index.sol
+++ contracts/races/params/Index.sol
@@ -22,9 +22,21 @@ import '../../queues/interfaces/IStaminaCostOf.sol';
 
 contract Params is Ownable, Withdrawable {
     
-    event NewRace(uint256 indexed id, uint256 indexed queueId, Race.Struct race);
-    event NewFinishedRace(uint256 indexed id, uint256 indexed queueId, Race.Struct race);
-    event UploadRace(uint256 indexed id, uint256 indexed queueId, Race.Struct race);
+    event NewRace(
+        uint256 indexed id, 
+        uint256 indexed queueId, 
+        Race.Struct race
+    );
+    event NewFinishedRace(
+        uint256 indexed id, 
+        uint256 indexed queueId, 
+        Race.Struct race
+    );
+    event UploadRace(
+        uint256 indexed id, 
+        uint256 indexed queueId, 
+        Race.Struct race
+    );
 
     uint256 public id = 1;
     RacesConstructor.Struct public control;
