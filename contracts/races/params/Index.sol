53c0d689526c750d948a8e220c2bab2eceb3e6f5
DIIMIIM <emilian@stater.co>
2022-10-27 22:39:39 +0300

    useless event removed

--- contracts/races/params/Index.sol
+++ contracts/races/params/Index.sol
@@ -21,11 +21,6 @@ contract Params is Whitelist {
         uint256 indexed queueId, 
         Race.Struct race
     );
-    event NewFinishedRace(
-        uint256 indexed id, 
-        uint256 indexed queueId, 
-        Race.Struct race
-    );
     event UploadRace(
         uint256 indexed id, 
         uint256 indexed queueId, 
