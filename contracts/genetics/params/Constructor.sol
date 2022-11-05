94f642f9e6649c241a06503ecf3c026332f7e81d
DIIMIIM <emilian@stater.co>
2022-11-05 13:30:56 +0200

    removed useless genetics constructor variables

--- contracts/genetics/params/Constructor.sol
+++ contracts/genetics/params/Constructor.sol
@@ -5,9 +5,6 @@ pragma solidity 0.8.17;
 library GeneticsConstructor {
     
     struct Struct {
-        uint32[72] male; // to delete
-        uint32[72] female; // to delete
-
         uint32 maleGenesProbability;
         uint32 femaleGenesProbability;
         uint32[13] geneticSequenceSignature;
